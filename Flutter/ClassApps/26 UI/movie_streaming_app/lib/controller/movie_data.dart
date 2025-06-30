169562affa1170eb584fbbce2ef0b32b09455300
Smitesh Bhore <136716222+Smiteshbhore21@users.noreply.github.com>
2025-06-30 17:17:34 +0530

    Movie Streaming APP

--- Flutter/ClassApps/26 UI/movie_streaming_app/lib/controller/movie_data.dart
+++ Flutter/ClassApps/26 UI/movie_streaming_app/lib/controller/movie_data.dart
@@ -14,14 +14,14 @@ class MovieData extends GetxController {
 
   Future<void> fetchData() async {
     String api =
-        "https://api.watchmode.com/v1/list-titles/?apiKey=n62pRwIPxth5SVzUU9U4PfSGz9CjbxhJGWATADmk&source_ids=203,57";
+        "https://api.watchmode.com/v1/list-titles/?apiKey=YOUR_API_KEY&source_ids=203,57"; // Add Your API Key
     Uri url = Uri.parse(api);
     http.Response responseData = await http.get(url);
     allTitles = json.decode(responseData.body);
     log("${allTitles["titles"].length}");
     for (int i = 0; i < 11; i++) {
       Uri url = Uri.parse(
-          "https://api.watchmode.com/v1/title/${allTitles["titles"][i]["id"]}/details/?apiKey=n62pRwIPxth5SVzUU9U4PfSGz9CjbxhJGWATADmk&append_to_response=sources");
+          "https://api.watchmode.com/v1/title/${allTitles["titles"][i]["id"]}/details/?apiKey=YOUR_API_KEY&append_to_response=sources");
       http.Response responseData = await http.get(url);
       allMovie.add(json.decode(responseData.body));
       if (allMovie[i]["user_rating"] != null &&
